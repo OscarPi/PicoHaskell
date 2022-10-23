b52ab464c2e38597bd6f9accadeffbdb2081fd0d
Oscar Hill <OscarPi@users.noreply.github.com>
2022-10-23 22:07:23 +0100

    Set up project structure with simple example lexer and parser.

---
+++ src/parser/driver.cpp
@@ -0,0 +1,19 @@
+#include "parser/driver.hpp"
+#include "parser/parser.hpp"
+
+Driver::Driver () : trace_parsing (false), trace_scanning (false) {
+    variables["one"] = 1;
+    variables["two"] = 2;
+}
+
+int Driver::parse (const std::string &f)
+{
+    file = f;
+    location.initialize(&file);
+    scan_begin();
+    yy::parser parse(*this);
+    parse.set_debug_level(trace_parsing);
+    int res = parse();
+    scan_end();
+    return res;
+}
