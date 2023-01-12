fcbe47896e428079a2003382073ab479850e143f
Oscar Hill <OscarPi@users.noreply.github.com>
2023-01-12 22:24:57 +0000

    Refactor code generation.

--- src/generation/generation.cpp
+++ src/generation/generation.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <sstream>
 #include "generation/generation.hpp"
 
 std::string sanitise_name(std::string name) {
@@ -55,8 +56,6 @@ void generate_standard_constructor_info_tables_and_closures(
         output << ".align 4 @ info table for " << name << std::endl;
         output << ".word 0 @ evacuation code" << std::endl;
         output << ".word 0 @ scavenge code" << std::endl;
-        output << ".word " << constructor.arity << " @ number of pointer words" << std::endl;
-        output << ".word 0 @ number of non-pointer words" << std::endl;
         output << ".thumb_func" << std::endl;
         output << sanitise_name(name) << "_standard_entry_code:" << std::endl;
         output << "    ADD R1, #4" << std::endl;
@@ -71,6 +70,9 @@ void generate_standard_constructor_info_tables_and_closures(
         }
     }
 
+    output << ".align 4 @ info table for literals" << std::endl;
+    output << ".word 0 @ evacuation code" << std::endl;
+    output << ".word 0 @ scavenge code" << std::endl;
     output << ".thumb_func" << std::endl;
     output << ".literal_standard_entry_code:" << std::endl;
     output << "    ADD R1, #4" << std::endl;
@@ -78,36 +80,75 @@ void generate_standard_constructor_info_tables_and_closures(
     output << "    BX R6 @ jump to return address" << std::endl;
 }
 
+void generate_info_table(
+        const std::string &name,
+        const std::unique_ptr<STGLambdaForm> &lambda_form,
+        std::ostream &output) {
+    output << ".align 4 @ info table for " << name << std::endl;
+    output << ".word 0 @ evacuation code" << std::endl;
+    output << ".word 0 @ scavenge code" << std::endl;
+    output << sanitise_name(name) << "_standard_entry_code:" << std::endl;
+
+    //ARGUMENT SATISFACTION CHECK
+
+    //STACK OVERFLOW CHECK
+
+    //HEAP OVERFLOW CHECK
+
+    //INFO POINTER UPDATE
+
+    //UPDATE FRAME CONSTRUCTION
+
+    switch (lambda_form->expr->get_form()) {
+
+    }
+}
+
 void generate_code_for_bindings(const std::unique_ptr<STGProgram> &program, std::ostream &output) {
+    std::stringstream data_section;
+
+    data_section << ".align 4" << std::endl;
+    data_section << ".data" << std::endl;
+
     for (const auto &[name, lambda_form]: program->bindings) {
-        if (lambda_form->argument_variables.empty()) {
-            if (lambda_form->expr->get_form() == stgform::constructor) {
-                auto constructor = dynamic_cast<STGConstructor*>(lambda_form->expr.get());
-                if (constructor->arguments.empty()) {
-                    output << sanitise_name(name) << "_closure = " << sanitise_name(constructor->constructor_name) << "_closure" << std::endl;
-                } else {
-                    output << ".align 4" << std::endl;
-                    output << sanitise_name(name) << "_closure:" << std::endl;
-                    output << ".word " << sanitise_name(constructor->constructor_name) << "_standard_entry_code @ info pointer" << std::endl;
-                    for (const std::string &arg: constructor->arguments) {
-                        output << ".word " << sanitise_name(arg) << "_closure @ arg" << std::endl;
-                    }
-                }
-            } else if (lambda_form->expr->get_form() == stgform::literal) {
-                auto literal = dynamic_cast<STGLiteral*>(lambda_form->expr.get());
+        if (lambda_form->argument_variables.empty() && lambda_form->expr->get_form() == stgform::constructor) {
+            auto constructor = dynamic_cast<STGConstructor *>(lambda_form->expr.get());
+            if (constructor->arguments.empty()) {
+                output << sanitise_name(name) << "_closure = "
+                       << sanitise_name(constructor->constructor_name) << "_closure" << std::endl;
+            } else {
                 output << ".align 4" << std::endl;
                 output << sanitise_name(name) << "_closure:" << std::endl;
-                output << ".word .literal_standard_entry_code @ info pointer" << std::endl;
-                if (std::holds_alternative<int>(literal->value)) {
-                    output << ".word " << std::get<int>(literal->value) << std::endl;
-                } else if (std::holds_alternative<char>(literal->value)) {
-                    output << ".word " << ((int) std::get<char>(literal->value)) << std::endl;
+                output << ".word " << sanitise_name(constructor->constructor_name)
+                       << "_standard_entry_code @ info pointer" << std::endl;
+                for (const std::string &arg: constructor->arguments) {
+                    output << ".word " << sanitise_name(arg) << "_closure @ arg" << std::endl;
                 }
             }
+        } else if (lambda_form->argument_variables.empty() && lambda_form->expr->get_form() == stgform::literal){
+            auto literal = dynamic_cast<STGLiteral *>(lambda_form->expr.get());
+            output << ".align 4" << std::endl;
+            output << sanitise_name(name) << "_closure:" << std::endl;
+            output << ".word .literal_standard_entry_code @ info pointer" << std::endl;
+            if (std::holds_alternative<int>(literal->value)) {
+                output << ".word " << std::get<int>(literal->value) << std::endl;
+            } else if (std::holds_alternative<char>(literal->value)) {
+                output << ".word " << ((int) std::get<char>(literal->value)) << std::endl;
+            }
         } else {
-
+            generate_info_table(name, lambda_form, output);
+            if (lambda_form->updatable) {
+                data_section << sanitise_name(name) << "_closure:" << std::endl;
+                data_section << ".word " << sanitise_name(name) << "_standard_entry_code @ info pointer" << std::endl;
+                data_section << ".word 0 @ empty word to hold indirection" << std::endl;
+            } else {
+                output << sanitise_name(name) << "_closure:" << std::endl;
+                output << ".word " << sanitise_name(name) << "_standard_entry_code @ info pointer" << std::endl;
+            }
         }
     }
+
+    output << data_section.str() << std::endl;
 }
 
 void generate_target_code(const std::unique_ptr<STGProgram> &program, std::ostream &output) {
@@ -120,6 +161,8 @@ void generate_target_code(const std::unique_ptr<STGProgram> &program, std::ostre
     output << "    MOV R6, R10" << std::endl;
     output << "    MOV R7, R11" << std::endl;
     output << "    PUSH {R4, R5, R6, R7} @ we will use lots of registers" << std::endl;
+    output << "    MOV R8, R0" << std::endl;
+    output << "    MOV R9, R1 @ initialise stack base registers" << std::endl;
     output << "    LDR R6, =.main_return" << std::endl;
     output << "    STR R6, [R1]" << std::endl;
     output << "    SUB R1, #4 @ push return address on B stack" << std::endl;
